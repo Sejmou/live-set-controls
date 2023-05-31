c885230e1c23ab6145b8a1e47ec661bdec221dd4
Sejmou <samo.kolter@gmail.com>
2023-05-31 10:42:25 +0200

    add Tailwind, change config stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,44 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import { invoke } from '@tauri-apps/api/tauri';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }));
   }
 
   return (
-    <div className="container">
+    <div className="flex flex-col items-center content-center h-full border">
       <h1>Welcome to Tauri!</h1>
 
-      <div className="row">
+      <div className="flex flex-row bg-purple-400">
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+          <img src="/vite.svg" className="w-16 h-16" alt="Vite logo" />
         </a>
         <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+          <img src="/tauri.svg" className="w-16 h-16" alt="Tauri logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={reactLogo} className="w-16 h-16" alt="React logo" />
         </a>
       </div>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
-        className="row"
-        onSubmit={(e) => {
+        className="flex flex-row"
+        onSubmit={e => {
           e.preventDefault();
           greet();
         }}
       >
         <input
           id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
+          onChange={e => setName(e.currentTarget.value)}
           placeholder="Enter a name..."
         />
         <button type="submit">Greet</button>
