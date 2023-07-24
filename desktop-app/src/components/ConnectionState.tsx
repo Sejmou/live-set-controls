8e17734f990c18f994fd6848880f574ada94a7c7
Sejmou <samo.kolter@gmail.com>
2023-07-24 12:50:51 +0200

    switch to pnpm, add Svelte (no proper desktop app yet)

--- desktop-app/src/components/ConnectionState.tsx
+++
@@ -1,9 +0,0 @@
-type Props = {
-  isConnected: boolean;
-};
-
-const ConnectionState = ({ isConnected }: Props) => {
-  return <p>State: {isConnected ? 'Connected' : 'Not connected'}</p>;
-};
-
-export default ConnectionState;
--- desktop-app/src/server-connection.ts
+++
@@ -1,6 +0,0 @@
-import { io } from 'socket.io-client';
-import { createClientAPI } from 'websocket-api';
-const URL = 'http://localhost:3000';
-
-export const socket = io(URL);
-export const api = createClientAPI(socket);
